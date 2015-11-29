2de63bee2746f060d3001033999f272b2f376cfb
lc1915 <liuchang1915@gmail.com>
2015-11-29 16:04:51 +0800

    finish ex3

--- week4/ex3/predict.m
+++ week4/ex3/predict.m
@@ -22,11 +22,11 @@ p = zeros(size(X, 1), 1);
 %
 
 
-
-
-
-
-
+temp1 = [ones(m, 1) X];
+temp2 = [ones(m, 1) sigmoid(temp1 * Theta1')];
+temp3 = sigmoid(temp2 * Theta2');
+[maxTemp3, maxTemp3_2] = max(temp3');
+p = maxTemp3_2';
 
 
 % =========================================================================
