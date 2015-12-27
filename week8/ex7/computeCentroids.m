b23e62f75bc5ef7c187aa2819d64afb186e4be3a
lc1915 <liuchang1915@gmail.com>
2015-12-27 21:13:16 +0800

    finish exercise 7

--- week8/ex7/computeCentroids.m
+++ week8/ex7/computeCentroids.m
@@ -26,12 +26,17 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
+for i = 1:K
+    count = 0;
+    for j = 1:m
+        if idx(j) == i
+            count = count + 1;
+            centroids(i,:) = centroids(i,:) + X(j,:);
+        end
+    end
 
-
-
-
-
-
+    centroids(i,:) = centroids(i,:) / count;
+end
 
 % =============================================================
 
--- week8/ex7/findClosestCentroids.m
+++ week8/ex7/findClosestCentroids.m
@@ -21,11 +21,17 @@ idx = zeros(size(X,1), 1);
 % Note: You can use a for-loop over the examples to compute this.
 %
 
+for i = 1:length(X)
+    minDist = Inf;
+    for j = 1:K
+        dist = norm(X(i,:) - centroids(j,:)) ^ 2;
 
-
-
-
-
+        if dist < minDist
+            minDist = dist;
+            idx(i) = j;
+        end
+    end
+end
 
 % =============================================================
 
